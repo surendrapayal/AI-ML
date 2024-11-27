83c5f57b6d600e1bb6141552fffd21ed2b2f38dc
rahulurane <rahulurane@gmail.com>
2024-11-27 12:54:24 +0530

    jira integration

--- gnoc_automation_flow/src/gnoc_automation_flow/tools/custom_tool.py
+++ gnoc_automation_flow/src/gnoc_automation_flow/tools/custom_tool.py
@@ -1,7 +1,19 @@
 from crewai_tools import tool
 from pydantic import BaseModel, Field
 # from gnoc_automation_flow.src.gnoc_automation_flow.types import JiraModel
-
+from dotenv import load_dotenv
+from jira import JIRA
+from crewai_tools import tool
+from sympy import print_mathml
+import os
+# loading variables from .env file
+load_dotenv()
+# Jira credentials and endpoint
+jira_url = 'https://rahuluraneai.atlassian.net/'
+api_token = os.getenv("JIRA_API_TOKEN")  # You can generate this from your Atlassian account
+email = 'rahulurane.ai@gmail.com'  # Your Jira login email
+project_key = 'Gnoc-Issue-tracker'  # Replace with your Jira project key
+issue_type = 'Bug'  # Issue type (Bug in this case)
 
 class MyCustomJiraToolInput(BaseModel):
     """Input schema for MyCustomJiraTool"""
@@ -11,13 +23,38 @@ class MyCustomJiraToolInput(BaseModel):
 
 
 @tool
-def my_custom_jira_tool_new(custom_input: MyCustomJiraToolInput) -> dict[str, int | str]:
-    """A custom tool that processes multiple inputs."""
+def my_custom_jira_tool_new(custom_input: MyCustomJiraToolInput):
+    """
+        Create a Jira ticket with the given details.
+
+        use issue-data as a request body for jira ticket creation
+    """
     print(f"priority inside jira tool:- {custom_input.priority}")
     print(f"description inside jira tool:- {custom_input.description}")
-    result_payload = {
-        "jiraId": 123456,
-        "jiraDescription": custom_input.description
+    # Create the Jira issue payload (JSON body)
+    issue_data = {
+         'project': {
+             'id': '10000'
+            },
+            'summary': f'Bug summary example {custom_input.priority}',
+            'description': custom_input.description,
+            'issuetype': {
+                'name': issue_type
+            }
     }
+
+    jiraOptions = {'server': jira_url}  # Change this to refer to your Jira URL
+    try:
+        jira = JIRA(options=jiraOptions, basic_auth=(email,api_token))
+        print(f"issue_data : {issue_data}")
+        response =jira.create_issue(fields=issue_data)
+        print(f"Jira Output:- {response}")
+        result_payload = {
+            "jiraId": response,
+            "jiraDescription": custom_input.description
+        }
+    except Exception as e:
+        print(f"Failed to create ticket: {e}")
     print(f"JSON Output:- {result_payload}")
     return result_payload
+
